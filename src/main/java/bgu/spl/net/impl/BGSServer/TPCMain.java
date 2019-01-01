e2de7e8fc659a2316fbb506cb72f7c95255fdf27
liad <liadna@post.bgu.ac.il>
2019-01-02 00:34:41 +0200

    process

--- src/main/java/bgu/spl/net/impl/BGSServer/TPCMain.java
+++ src/main/java/bgu/spl/net/impl/BGSServer/TPCMain.java
@@ -1,11 +1,11 @@
-package bgu.spl.net.impl;
+package bgu.spl.net.impl.BGSServer;
 
 import bgu.spl.net.api.MessageEncoderDecoderImp;
 import bgu.spl.net.api.bidi.BidiMessagingProtocolImp;
 import bgu.spl.net.api.bidi.SharedData;
 import bgu.spl.net.srv.Server;
 
-public class ReactorMain {
+public class TPCMain {
     public static void main(String[] args) {
         SharedData shared = new SharedData();
         Server.threadPerClient(Integer.parseInt(args[0]), ()-> new BidiMessagingProtocolImp(shared), ()-> new MessageEncoderDecoderImp()).serve();
